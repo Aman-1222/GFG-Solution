367fffcc15746f03691a07b650689423559cd3f4
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2025-04-25 21:25:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Largest Element in Array/largest-element-in-array.java
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.java
@@ -55,8 +55,7 @@ class Solution {
         // code here
         int ans=0;
         for(int i=0;i<arr.length;i++)
-            if(arr[i]>ans) ans=arr[i];
+            if(arr[i]> ans) ans=arr[i];
         return ans;
-        
     }
 }
