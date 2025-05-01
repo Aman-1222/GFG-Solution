cc19b0bc6534b69eb2a23de2e3aee676c287c140
Aman Prakash <128574278+Aman-1222@users.noreply.github.com>
2025-05-01 20:44:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Armstrong Numbers/armstrong-numbers.java
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int n = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            boolean flag = ob.armstrongNumber(n);
+            if (flag) {
+                System.out.println("true");
+
+            } else {
+                System.out.println("false");
+            }
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    static boolean armstrongNumber(int n) {
+        // code here
+        int cnt=(int)Math.log10(n)+1;
+        int temp=n;
+        int num=0;
+        while(temp>0){
+            num+=Math.pow((temp%10),cnt);
+            temp/=10;
+        }
+        return (num==n)? true : false;
+    }
+}
